64f9657d73ab54f690e1b2e4967c1436bc503244
beavillaflor <125416318+beavillaflor@users.noreply.github.com>
2026-01-20 15:21:21 -0600

    Update hover.tsx

--- app/components/hover.tsx
+++ app/components/hover.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import React from "react";
 import "../styles/custom.css";
+
+
+type HoverDefProps = {
+  term: string;
+  definition: string;
+};
+
 
 export default function HoverDef({ term, definition }) {
   return (
