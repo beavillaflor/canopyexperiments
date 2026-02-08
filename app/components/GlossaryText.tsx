8fd649c832997870f877cbf1cee0db32c84b161a
beavillaflor <125416318+beavillaflor@users.noreply.github.com>
2026-02-07 21:50:43 -0600

    Update GlossaryText.tsx

--- app/components/GlossaryText.tsx
+++ app/components/GlossaryText.tsx
@@ -1,10 +1,1 @@
-import React from "react";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function GlossaryText({ children }: Props) {
-  // Render children verbatim during SSR
-  return <>{children}</>;
-}
